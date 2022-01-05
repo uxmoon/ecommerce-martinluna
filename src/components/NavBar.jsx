42f6fb569cfa62a125f52550901d4a707305e51b
Martin Luna <uxmoon@gmail.com>
2022-01-04 21:18:01 -0300

    Se crea componente de navegacion

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Productos</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
