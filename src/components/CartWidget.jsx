cc181acb5f43d5c61639848a0c5b9556d126a526
Martin Luna <maluna@technisys.com>
2022-01-07 11:00:23 -0300

    Se crea componente CartWidget

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import { ReactComponent as IconCart } from "../cart.svg";
+import "./CartWidget.css";
+
+const CartWidget = () => (
+  <div className="CartWidget">
+    <IconCart /> 4
+  </div>
+);
+
+export default CartWidget;
