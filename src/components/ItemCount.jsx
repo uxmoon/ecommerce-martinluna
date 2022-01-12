7ed806ac900376a2a243b86dcb306e13aeaef5d2
Martin Luna <maluna@technisys.com>
2022-01-12 11:18:47 -0300

    Se agregan funciones para agregar/quitar productos

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
 
 export const ItemCount = () => {
-  const [contador, setContador] = useState(0);
+  // Inicio estado con un producto seleccionado por defecto
+  const [contador, setContador] = useState(1);
+
+  // Configuro el stock disponible
+  const stockDisponible = 10;
+
+  // Funciones para actualizar el estado
   const sumar = () => {
-    setContador((contador) => contador + 1);
+    // No debe superar el stock disponible
+    if (contador < stockDisponible) {
+      setContador((contador) => contador + 1);
+    }
   };
   const restar = () => {
-    setContador((contador) => contador - 1);
+    // No debe ser menor a 1 producto
+    if (contador > 1) {
+      setContador((contador) => contador - 1);
+    }
   };
+
+  /* Muestro un mensaje con la cantidad de productos */
+  const agregarProducto = () => {
+    alert(`Se agregaron ${contador} productos al carrito`);
+  };
+
   return (
     <div>
+      {/* Muestro el stock disponible */}
+      <p>Stock disponible: {stockDisponible}</p>
+
+      {/* Botones para agrega o quitar productos */}
       <button onClick={restar}>restar</button>
       {contador}
       <button onClick={sumar}>sumar</button>
+
+      <button onClick={agregarProducto}>Agregar al carrito</button>
     </div>
   );
 };
