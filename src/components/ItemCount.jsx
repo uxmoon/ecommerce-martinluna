90cbd81e27996887cd9d2267f87ba753a91c0e6e
Martin Luna <maluna@technisys.com>
2022-01-11 21:14:56 -0300

    Se agrega useState para el contador de clicks

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
+
 export const ItemCount = () => {
+  const [contador, setContador] = useState(0);
+  const sumar = () => {
+    setContador((contador) => contador + 1);
+  };
+  const restar = () => {
+    setContador((contador) => contador - 1);
+  };
   return (
     <div>
-      <button>restar</button>
-      contador
-      <button>sumar</button>
+      <button onClick={restar}>restar</button>
+      {contador}
+      <button onClick={sumar}>sumar</button>
     </div>
   );
 };
