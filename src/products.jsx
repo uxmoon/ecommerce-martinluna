53b09497dd1273c350aa1cadb700f2ad9239d18b
Martin Luna <maluna@technisys.com>
2022-01-14 11:51:44 -0300

    Mock de productos de ejemplo

---
+++ src/products.jsx
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    title: 'Smart TV Philips 50"',
+    description: [
+      "Su resolución es 4K.",
+      "Tecnología HDR para una calidad de imagen mejorada.",
+      "Modo de sonido: Dolby Atmos, AI Sound.",
+    ],
+    price: "$ 69.999",
+    pictureUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_826710-MLA42164194325_062020-O.webp",
+  },
+  {
+    id: 2,
+    title: 'Smart TV Samsung Series 7 LED 4K 50"',
+    description: [
+      "Su resolución es 4K.",
+      "Tecnología HDR para una calidad de imagen mejorada.",
+      "Modo de sonido: Dolby Digital Plus, Q-Symphony.",
+    ],
+    price: "$ 72.999",
+    pictureUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_787221-MLA48007684849_102021-O.webp",
+  },
+  {
+    id: 3,
+    title: 'Smart TV TCL 50P715 LED 4K 50"',
+    description: [
+      "Su resolución es 4K.",
+      "Tecnología HDR para una calidad de imagen mejorada.",
+      "Modo de sonido: Dolby Audio.",
+    ],
+    price: "$ 79.999",
+    pictureUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_711131-MLA46112698238_052021-O.webp",
+  },
+];
