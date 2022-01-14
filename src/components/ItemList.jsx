93ada8757c9438320bbc259ed8bbdedd305e8cf8
Martin Luna <maluna@technisys.com>
2022-01-14 12:06:05 -0300

    Ternario para loading y mostrar productos

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,18 +4,20 @@ import Item from "./Item";
 
 function ItemList() {
   const [productos, setProductos] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const promesaProductos = new Promise((response, reject) => {
       setTimeout(() => {
         response(data);
-        // reject("Se produjo un error")
+        // reject("Se produjo un error");
       }, 2000);
     });
 
     promesaProductos
       .then((res) => {
         setProductos(res);
+        setIsLoaded(true);
       })
       .catch((err) => {
         console.log(err);
@@ -24,10 +26,16 @@ function ItemList() {
 
   return (
     <div>
-      <p>Item List</p>
-      {productos.map((producto) => (
-        <Item key={producto.id} producto={producto} />
-      ))}
+      <p>Listado de productos</p>
+      {isLoaded ? (
+        <>
+          {productos.map((producto) => (
+            <Item key={producto.id} producto={producto} />
+          ))}
+        </>
+      ) : (
+        <p>Cargando productos...</p>
+      )}
     </div>
   );
 }
