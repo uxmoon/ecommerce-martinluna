9f7d7009e9a587bad4c69960c11459a7292eb361
Martin Luna <maluna@technisys.com>
2022-01-14 11:45:20 -0300

    Agrego nuevo componente ItemList

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import "./ItemListContainer.css";
+import ItemList from "./ItemList";
 
 const ItemListContainer = ({ greeting = "invitado" }) => {
   return (
     <div className="ItemListContainer">
       <div className="ItemListContainer-container">
         <h2>Hola {greeting}!</h2>
+        <ItemList />
       </div>
     </div>
   );
