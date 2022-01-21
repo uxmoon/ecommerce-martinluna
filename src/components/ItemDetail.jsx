827a3326a376ca165be6990b5e61e230aeb67467
Martin Luna <maluna@technisys.com>
2022-01-20 21:11:07 -0300

    Se destructuran props para el detalle del item

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import "./ItemDetail.css";
 
-export default function ItemDetail(props) {
+export default function ItemDetail({ title, price, thumbnail }) {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.price}</p>
-      <img src={props.thumbnail} alt={props.title} />
+    <div className="ItemDetail">
+      <div className="ItemDetail-thumbnail">
+        <img src={thumbnail} alt={title} />
+      </div>
+      <div className="ItemDetail-content">
+        <h2 className="ItemDetail-title">{title}</h2>
+        <p className="ItemDetail-price">$ {price}</p>
+      </div>
     </div>
   );
 }
