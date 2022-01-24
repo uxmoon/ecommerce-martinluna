fc5ff3f4a31859e39f7167a6caf971be0e440150
Martin Luna <maluna@technisys.com>
2022-01-24 15:55:19 -0300

    Agrego componentes de router para primera prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import { ItemCount } from "./components/ItemCount";
+import Home from "./components/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>eCommerce</h1>
-        <NavBar />
-        <ItemListContainer />
-        <ItemDetailContainer />
-        <ItemCount stock={100} initial={1} />
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    // <div className="App">
+    //   <header>
+    //     <h1>eCommerce</h1>
+    //     <NavBar />
+    //     <ItemListContainer />
+    //     <ItemDetailContainer />
+    //     <ItemCount stock={100} initial={1} />
+    //   </header>
+    // </div>
   );
 }
 
