eecdba3066465effdac07b7b44abf29159dc088b
Martin Luna <maluna@technisys.com>
2022-01-24 16:27:18 -0300

    Se agregan archivos para rutas Productos y Carro

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Home() {
+export default function Cart() {
   return (
     <div>
-      <h1>eCommerce</h1>
+      <h1>Carrito de compras</h1>
     </div>
   );
 }
