4d3eb683a1529572b1e16caeaa4cd661c274f976
Martin Luna <maluna@technisys.com>
2022-01-25 20:09:31 -0300

    Se comenta el filter

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,12 @@
+import ItemList from "./ItemList";
+import "./ItemListContainer.css";
 // import React, { useState, useEffect } from "react";
 // import { useParams } from "react-router-dom";
-import ItemList from "./ItemList";
 // import { data } from "../products";
-import "./ItemListContainer.css";
 
 const ItemListContainer = () => {
   // const { categoryId } = useParams();
   // const [arrayProductos, setArrayProductos] = useState([]);
-
   // useEffect(() => {
   //   setArrayProductos(data.filter((item) => item.categoryId === categoryId));
   // }, [categoryId]);
