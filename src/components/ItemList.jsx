d17af112f95e71eeb460f298e40b66f42f692db9
Martin Luna <maluna@technisys.com>
2022-01-25 19:18:48 -0300

    Se agrega useParams para filtrar la categoria

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { data } from "../products";
 import Item from "./Item";
 import "./ItemList.css";
 
 function ItemList() {
+  const { categoryId } = useParams();
+
   const [productos, setProductos] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [message, setMessage] = useState(false);
@@ -25,7 +28,11 @@ function ItemList() {
         console.log(err);
         setMessage(true);
       });
-  });
+  }, []);
+
+  useEffect(() => {
+    setProductos(data.filter((item) => item.categoryId === categoryId));
+  }, [categoryId]);
 
   const listadoProductos = productos.map((producto) => (
     <Item key={producto.id} producto={producto} />
@@ -33,7 +40,7 @@ function ItemList() {
 
   return (
     <div>
-      <p>Listado de productos</p>
+      <h1>Productos</h1>
       {isLoaded ? (
         <div className="ItemList">{listadoProductos}</div>
       ) : (
