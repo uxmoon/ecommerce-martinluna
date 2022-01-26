168d0b32080419999629be6de38f2ac4c7e9ee60
Martin Luna <maluna@technisys.com>
2022-01-26 15:46:59 -0300

    Se quita todo el codigo comentado

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,22 +1,10 @@
 import ItemList from "./ItemList";
 import "./ItemListContainer.css";
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { data } from "../products";
 
 const ItemListContainer = () => {
-  // const { categoryId } = useParams();
-  // const [arrayProductos, setArrayProductos] = useState([]);
-  // useEffect(() => {
-  //   setArrayProductos(data.filter((item) => item.categoryId === categoryId));
-  // }, [categoryId]);
-
   return (
     <div className="ItemListContainer">
       <div className="ItemListContainer-container">
-        {/* {arrayProductos.map((item) => (
-          <p>{item.title}</p>
-        ))} */}
         <ItemList />
       </div>
     </div>
