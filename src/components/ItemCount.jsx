3b1ab10ab2c1e0372650f16e3a28cab665d59bbd
Martin Luna <maluna@technisys.com>
2022-02-02 15:56:55 -0300

    Atributos de accesibilidad

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,66 +3,43 @@ import "./ItemCount.css";
 
 function ItemCount({ stock, initial, onAdd }) {
   const [contador, setContador] = useState(initial);
-  // const [disableButton, setDisableButton] = useState(true);
 
   const add = () => {
-    /**
-     * Resto hasta que no haya stock disponible
-     * Agrego 1 al contador interno
-     */
-    // if (stock > 0) {
-    //   setContador((contador) => contador + 1);
-    //   onAdd(1);
-    // }
     if (contador < stock) {
       setContador(contador + 1);
     }
   };
 
   const subtract = () => {
-    /**
-     * Sumo 1 al stock disponible
-     * Resto 1 al contador interno
-     */
-    // if (contador > 0) {
-    //   setContador((contador) => contador - 1);
-    //   onSubtract(1);
-    // }
     if (contador > initial) {
       setContador(contador - 1);
     }
   };
 
-  // useEffect(() => {
-  //   if (contador > 0) {
-  //     setDisableButton(false);
-  //   } else {
-  //     setDisableButton(true);
-  //   }
-  // }, [contador]);
-
   return (
     <div className="ItemCount">
-      {/* Muestro el stock disponible */}
       <p>Stock disponible: {stock}</p>
 
       <div className="ItemCount-actions">
-        {/* Botones para agrega o quitar productos */}
-        <button className="ItemCount-button" onClick={subtract}>
-          -
+        <button
+          className="Button Button--icon"
+          onClick={subtract}
+          aria-label="Quitar"
+        >
+          <span aria-hidden="true">&#8722;</span>
         </button>
         <div className="ItemCount-number">{contador}</div>
-        <button className="ItemCount-button" onClick={add}>
-          +
+        <button
+          className="Button Button--icon"
+          onClick={add}
+          aria-label="Agregar"
+        >
+          <span aria-hidden="true">&#43;</span>
         </button>
       </div>
 
-      <button
-        onClick={() => onAdd(contador)}
-        className="Button"
-        // disabled={disableButton}
-      >
-        Agregar al carrito
+      <button onClick={() => onAdd(contador)} className="Button">
+        Agregar al carro
       </button>
     </div>
   );
