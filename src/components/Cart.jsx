9b03c54c5c844b6fe19ecc3637aa7cad28127fc6
Martin Luna <maluna@technisys.com>
2022-02-02 12:54:49 -0300

    Condicional carro vacio y botones de quitar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,17 +2,28 @@ import React, { useContext } from "react";
 import { cartContext } from "../context/CartProvider";
 
 export default function Cart() {
-  const { cart, removeItem } = useContext(cartContext);
+  const { cart, removeItem, clear } = useContext(cartContext);
   console.log(cart);
   return (
     <div>
       <h1>Carro de compras</h1>
-      {cart.length === 0 && <p>No hay productos en el carro.</p>}
+      {cart.length === 0 ? (
+        <p>No hay productos en el carro.</p>
+      ) : (
+        <button className="Button Button--secondary" onClick={() => clear()}>
+          Quitar todos
+        </button>
+      )}
       {cart.map((product) => (
         <div key={product.item.id}>
           <p>{product.item.title}</p>
           <p>cantidad: {product.quantity}</p>
-          <button onClick={() => removeItem(product.item.id)}>Quitar</button>
+          <button
+            className="Button Button--secondary"
+            onClick={() => removeItem(product.item.id)}
+          >
+            Quitar
+          </button>
         </div>
       ))}
     </div>
