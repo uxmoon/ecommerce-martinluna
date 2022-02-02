c0e33e11768ba44cdd690a9ee778a1d56c87c415
Martin Luna <maluna@technisys.com>
2022-02-02 11:13:43 -0300

    Se agrega funcion para agregar item al carro

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -5,9 +5,14 @@ export const cartContext = createContext();
 export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+  const addItem = (item, quantity) => {
+    setCart([...cart, { item: item, quantity: quantity }]);
+  };
   return (
     <>
-      <cartContext.Provider value={{ cart }}>{children}</cartContext.Provider>
+      <cartContext.Provider value={{ cart, addItem }}>
+        {children}
+      </cartContext.Provider>
     </>
   );
 }
