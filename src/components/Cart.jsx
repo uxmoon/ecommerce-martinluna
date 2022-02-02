181637fd2b1c30606a8823b8a6d3ba166683ddfa
Martin Luna <maluna@technisys.com>
2022-02-02 13:16:48 -0300

    Se agregan estilos para centrar el contenido

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,31 +1,34 @@
 import React, { useContext } from "react";
 import { cartContext } from "../context/CartProvider";
+import "./Cart.css";
 
 export default function Cart() {
   const { cart, removeItem, clear } = useContext(cartContext);
   console.log(cart);
   return (
-    <div>
-      <h1>Carro de compras</h1>
-      {cart.length === 0 ? (
-        <p>No hay productos en el carro.</p>
-      ) : (
-        <button className="Button Button--secondary" onClick={() => clear()}>
-          Quitar todos
-        </button>
-      )}
-      {cart.map((product) => (
-        <div key={product.item.id}>
-          <p>{product.item.title}</p>
-          <p>cantidad: {product.quantity}</p>
-          <button
-            className="Button Button--secondary"
-            onClick={() => removeItem(product.item.id)}
-          >
-            Quitar
+    <div className="Cart">
+      <div className="Cart-container">
+        <h1>Carro de compras</h1>
+        {cart.length === 0 ? (
+          <p>No hay productos en el carro.</p>
+        ) : (
+          <button className="Button Button--secondary" onClick={() => clear()}>
+            Quitar todos
           </button>
-        </div>
-      ))}
+        )}
+        {cart.map((product) => (
+          <div key={product.item.id}>
+            <p>{product.item.title}</p>
+            <p>cantidad: {product.quantity}</p>
+            <button
+              className="Button Button--secondary"
+              onClick={() => removeItem(product.item.id)}
+            >
+              Quitar
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
