3667bb173a03003f1439df810c76a819ff286aa5
Martin Luna <maluna@technisys.com>
2022-02-02 12:17:15 -0300

    Actualizo la cantidad del producto buscando el index

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -6,8 +6,20 @@ export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addItem = (item, quantity) => {
-    setCart([...cart, { item: item, quantity: quantity }]);
+    if (isInCart(item.id)) {
+      const indexItem = cart.findIndex((el) => el.item.id === item.id);
+      // console.log(indexItem);
+      cart[indexItem].quantity = cart[indexItem].quantity + quantity;
+      setCart([...cart]);
+    } else {
+      setCart([...cart, { item: item, quantity: quantity }]);
+    }
   };
+
+  const isInCart = (itemId) => {
+    return cart.some((el) => el.item.id === itemId);
+  };
+
   return (
     <>
       <cartContext.Provider value={{ cart, addItem }}>
