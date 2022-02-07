d027b2fe7198a501d14f2b0f51eac3b9bd837b96
Martin Luna <maluna@technisys.com>
2022-02-07 19:17:39 -0300

    Cantidad de items en el carro

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,19 @@
+import React, { useContext } from "react";
 import { ReactComponent as IconCart } from "../images/cart.svg";
 import { Link } from "react-router-dom";
+import { cartContext } from "../context/CartProvider";
 import "./CartWidget.css";
 
-const CartWidget = () => (
-  <Link to="/cart" className="CartWidget">
-    <IconCart /> #
-  </Link>
-);
+const CartWidget = () => {
+  const { cart } = useContext(cartContext);
+  const totalItems = cart.reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
+  return (
+    <Link to="/cart" className="CartWidget">
+      <IconCart /> {totalItems}
+    </Link>
+  );
+};
 
 export default CartWidget;
