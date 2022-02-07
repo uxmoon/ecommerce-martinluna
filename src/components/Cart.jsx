053502474225599c267da71d442800565afd8691
Martin Luna <maluna@technisys.com>
2022-02-07 20:08:04 -0300

    Se actualiza UI para Carro

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,24 +23,51 @@ export default function Cart() {
             </Link>
           </>
         ) : (
-          <button className="Button Button--secondary" onClick={() => clear()}>
-            Quitar todos
-          </button>
+          <div className="Cart-row" style={{ marginBottom: "1rem" }}>
+            <div></div>
+            <div>
+              <button
+                className="Button Button--secondary"
+                onClick={() => clear()}
+              >
+                Quitar todos
+              </button>
+            </div>
+          </div>
         )}
         {cart.map((product) => (
-          <div key={product.item.id}>
-            <h3>{product.item.title}</h3>
-            <p>$ {formatPrice(product.item.price)}</p>
-            <p>Unidades: {product.quantity}</p>
-            <button
-              className="Button Button--secondary"
-              onClick={() => removeItem(product.item.id)}
-            >
-              Quitar
-            </button>
+          <div key={product.item.id} className="Cart-item">
+            <div className="Cart-item-content">
+              <img src={product.item.pictureUrl} alt={product.item.title} />
+              <h3>{product.item.title}</h3>
+            </div>
+            <div className="Cart-item-actions">
+              <p>$ {formatPrice(product.item.price)}</p>
+              <p>
+                {product.quantity}{" "}
+                {product.quantity === 1 ? "unidad" : "unidades"}
+              </p>
+              <button
+                className="Button Button--secondary"
+                onClick={() => removeItem(product.item.id)}
+              >
+                Quitar
+              </button>
+            </div>
           </div>
         ))}
-        {totalPrice > 0 && <p>Total: $ {formatPrice(totalPrice)}</p>}
+        {totalPrice > 0 && (
+          <div className="Cart-row">
+            <div></div>
+            <div>
+              <p className="Cart-total">
+                Total
+                <br />
+                <strong>$ {formatPrice(totalPrice)}</strong>
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
