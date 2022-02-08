cdf86b2660e2690bc56d1a0396f9b5d206f1e813
Martin Luna <maluna@technisys.com>
2022-02-08 14:38:32 -0300

    Se cambia referencia para imagen

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,12 +3,12 @@ import { formatPrice } from "../helpers";
 import "./Item.css";
 
 function Item({ producto }) {
-  const { id, pictureUrl, title, price } = producto;
+  const { id, imageUrl, title, price } = producto;
 
   return (
     <div className="Item">
       <Link to={`/item/${id}`}>
-        <img src={pictureUrl} alt={title} />
+        <img src={imageUrl} alt={title} />
         <h2>{title}</h2>
       </Link>
       <p>$ {formatPrice(price)}</p>
