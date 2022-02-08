a64e1685e26147d63ca6b3b66ec400796fcc4733
Martin Luna <maluna@technisys.com>
2022-02-08 15:26:51 -0300

    Se agrega firebase para agregar detalle de producto

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
-import { data } from "../products";
+// import { data } from "../products";
 import { cartContext } from "../context/CartProvider";
+import { getFirestore } from "../firebase";
 import "./ItemDetailContainer.css";
 
 const ItemDetailContainer = () => {
@@ -12,18 +13,22 @@ const ItemDetailContainer = () => {
   const [producto, setProducto] = useState({});
   const [addedToCart, setAddedToCart] = useState(false);
 
-  const promesaProducto = new Promise((response, reject) => {
-    setTimeout(() => {
-      response(data);
-    }, 2000);
-  });
-
   useEffect(() => {
     setIsLoading(true);
 
-    promesaProducto
-      .then((res) => {
-        setProducto(res.find((item) => item.id === parseInt(itemId)));
+    const db = getFirestore();
+    const itemCollection = db.collection("items");
+    const miItem = itemCollection.doc(itemId);
+
+    miItem
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log("no existe ese documento");
+          return;
+        }
+        console.log("item found");
+        setProducto({ id: doc.id, ...doc.data() });
       })
       .catch((err) => {
         console.log(err);
@@ -32,6 +37,27 @@ const ItemDetailContainer = () => {
         setIsLoading(false);
       });
   }, []);
+
+  // const promesaProducto = new Promise((response, reject) => {
+  //   setTimeout(() => {
+  //     response(data);
+  //   }, 2000);
+  // });
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+
+  //   promesaProducto
+  //     .then((res) => {
+  //       setProducto(res.find((item) => item.id === parseInt(itemId)));
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   const onAddToCart = (contador) => {
     addItem(producto, contador);
