26bce03ef0308a4578d9c3e63730061136af683f
Martin Luna <maluna@technisys.com>
2022-02-08 15:24:58 -0300

    Se destructura producto prop

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,24 +1,20 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ItemCount from "./ItemCount";
 import { formatPrice } from "../helpers";
 import "./ItemDetail.css";
 
 export default function ItemDetail({ producto, onAdd, addedToCart }) {
+  const { imageUrl, title, price, description, stock } = producto;
+
   return (
     <div className="ItemDetail">
       <div className="ItemDetail-thumbnail">
-        <img src={producto.pictureUrl} alt={producto.title} />
+        <img src={imageUrl} alt={title} />
       </div>
       <div className="ItemDetail-content">
-        <h2 className="ItemDetail-title">{producto.title}</h2>
-        <p className="ItemDetail-price">
-          $ {producto.price && formatPrice(producto.price)}
-        </p>
-        <ul>
-          {producto.description &&
-            producto.description.map((item) => <li key={item}>{item}</li>)}
-        </ul>
+        <h2 className="ItemDetail-title">{title}</h2>
+        <p className="ItemDetail-price">$ {price && formatPrice(price)}</p>
+        <p>{description}</p>
       </div>
       {addedToCart ? (
         <div className="ItemCount ItemCount--added">
@@ -27,7 +23,7 @@ export default function ItemDetail({ producto, onAdd, addedToCart }) {
           </Link>
         </div>
       ) : (
-        <ItemCount stock={5} initial={1} onAdd={onAdd} />
+        <ItemCount stock={stock} initial={1} onAdd={onAdd} />
       )}
     </div>
   );
