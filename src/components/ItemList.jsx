573a25250ac78a0f9eb30763bc3d250cbf007b70
Martin Luna <maluna@technisys.com>
2022-02-08 15:26:24 -0300

    Se agrega firebase para listar productos

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { data } from "../products";
+// import { data } from "../products";
 import Item from "./Item";
+import { getFirestore } from "../firebase";
 import "./ItemList.css";
 
 function ItemList() {
@@ -11,21 +12,32 @@ function ItemList() {
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState(false);
 
-  const promesaProductos = new Promise((response, reject) => {
-    setTimeout(() => {
-      response(data);
-    }, 2000);
-  });
-
   useEffect(() => {
     setIsLoading(true);
 
-    promesaProductos
-      .then((res) => {
+    const db = getFirestore();
+    const itemCollection = db.collection("items");
+
+    itemCollection
+      .get()
+      .then((querySnapShot) => {
+        if (querySnapShot.size === 0) {
+          console.log("no hay documentos con en ese query");
+          return;
+        }
+        console.log("hay documentos");
+        // console.log(querySnapShot.docs.map((d) => d.data()));
+
         if (categoryId) {
-          setProductos(data.filter((item) => item.categoryId === categoryId));
+          setProductos(
+            querySnapShot.docs
+              .filter((doc) => doc.categoryId === categoryId)
+              .map((doc) => ({ id: doc.id, ...doc.data() }))
+          );
         } else {
-          setProductos(res);
+          setProductos(
+            querySnapShot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+          );
         }
       })
       .catch((err) => {
@@ -37,12 +49,41 @@ function ItemList() {
       });
   }, [categoryId]);
 
+  // const promesaProductos = new Promise((response, reject) => {
+  //   setTimeout(() => {
+  //     response(data);
+  //   }, 2000);
+  // });
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+
+  //   promesaProductos
+  //     .then((res) => {
+  //       if (categoryId) {
+  //         setProductos(data.filter((item) => item.categoryId === categoryId));
+  //       } else {
+  //         setProductos(res);
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       setMessage(true);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, [categoryId]);
+
   const listadoProductos = productos.map((producto) => (
     <Item key={producto.id} producto={producto} />
   ));
 
+  console.log(productos);
+
   return (
     <div>
+      {JSON.stringify(productos)}
       <h1>Productos</h1>
       {isLoading ? (
         <div className="loader">
