9b0ed9d876302b345e2a98f6cb8e94a90566e522
Martin Luna <maluna@technisys.com>
2022-02-09 17:18:28 -0300

    Se tailwind para navbar y widget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,17 +1,28 @@
 import React, { useContext } from "react";
-import { ReactComponent as IconCart } from "../images/cart.svg";
 import { Link } from "react-router-dom";
 import { cartContext } from "../context/CartProvider";
-// import "./CartWidget.css";
 
 const CartWidget = () => {
   const { cart } = useContext(cartContext);
+
   const totalItems = cart.reduce((total, item) => {
     return total + item.quantity;
   }, 0);
+
   return (
-    <Link to="/cart" className="CartWidget">
-      <IconCart /> {totalItems}
+    <Link
+      to="/cart"
+      className="hidden md:flex items-center space-x-2 bg-gray-600 text-white rounded px-2 py-1"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+      </svg>
+      <span>{totalItems}</span>
     </Link>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,57 @@
 import { NavLink } from "react-router-dom";
 import CartWidget from "./CartWidget";
-// import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      <div className="NavBar-container">
-        <p>
-          <NavLink to="/">Mi Tienda</NavLink>
-        </p>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/category/audio">Audio</NavLink>
-            </li>
-            <li>
-              <NavLink to="/category/computacion">Computación</NavLink>
-            </li>
-            <li>
-              <NavLink to="/category/televisores">Televisores</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <CartWidget />
+    <nav className="bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center py-2">
+          <div className="flex space-x-4">
+            {/* logo */}
+            <NavLink to="/" className="flex py-2 md:px-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              </svg>
+              <span className="font-bold">Mi Tienda</span>
+            </NavLink>
+
+            {/* Menu */}
+            <div className="hidden md:flex space-x-2">
+              <NavLink
+                to="/category/audio"
+                className="py-2 px-3 hover:bg-gray-600 hover:text-white rounded"
+              >
+                Audio
+              </NavLink>
+              <NavLink
+                to="/category/computacion"
+                className="py-2 px-3 hover:bg-gray-600 hover:text-white rounded"
+              >
+                Computación
+              </NavLink>
+              <NavLink
+                to="/category/televisores"
+                className="py-2 px-3 hover:bg-gray-600 hover:text-white rounded"
+              >
+                Televisores
+              </NavLink>
+            </div>
+          </div>
+          <CartWidget />
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
