5f0f2a6ddaa7073352f9f8911a91d5626128ee02
Martin Luna <maluna@technisys.com>
2022-02-10 11:52:12 -0300

    Se agrega Fn para cerrar menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import CartWidget from "./CartWidget";
+import { IconLogo, IconMenu, IconClose } from "./Icons";
 
 function NavBar() {
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <nav className="bg-gray-100">
       <div className="max-w-7xl mx-auto px-4">
@@ -11,20 +17,7 @@ function NavBar() {
           <div className="flex space-x-4">
             {/* logo */}
             <NavLink to="/" className="flex py-2 md:px-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
+              {IconLogo}
               <span className="font-bold">Mi Tienda</span>
             </NavLink>
 
@@ -53,34 +46,33 @@ function NavBar() {
           <CartWidget />
 
           <div className="md:hidden">
-            <button onClick={() => setToggleMenu(!toggleMenu)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+            <button onClick={handleToggle}>
+              {toggleMenu ? IconClose : IconMenu}
             </button>
           </div>
         </div>
       </div>
       {/* mobile menu */}
       <div className={`${toggleMenu ? "" : "hidden"} md:hidden`}>
-        <NavLink to="/category/audio" className="block py-2 px-4">
+        <NavLink
+          to="/category/audio"
+          className="block py-2 px-4"
+          onClick={handleToggle}
+        >
           Audio
         </NavLink>
-        <NavLink to="/category/computacion" className="block py-2 px-4">
+        <NavLink
+          to="/category/computacion"
+          className="block py-2 px-4"
+          onClick={handleToggle}
+        >
           Computación
         </NavLink>
-        <NavLink to="/category/televisores" className="block py-2 px-4">
+        <NavLink
+          to="/category/televisores"
+          className="block py-2 px-4"
+          onClick={handleToggle}
+        >
           Televisores
         </NavLink>
       </div>
