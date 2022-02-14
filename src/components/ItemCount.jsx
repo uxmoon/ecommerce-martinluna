11ef75f687ff5f2564c6db0ee0489e6b78d929fe
Martin Luna <maluna@technisys.com>
2022-02-14 11:43:28 -0300

    Estilos de botones y layout

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { IconMinus, IconPlus } from "./Icons";
 
 function ItemCount({ stock, initial, onAdd }) {
   const [contador, setContador] = useState(initial);
@@ -16,23 +17,25 @@ function ItemCount({ stock, initial, onAdd }) {
   };
 
   return (
-    <div>
+    <>
       <div className="flex items-center mb-8">
         <div className="flex items-center">
           <button
+            type="button"
             onClick={subtract}
             aria-label="Quitar"
-            className="bg-blue-600 text-white w-12 h-12 rounded shadow"
+            className="bg-white hover:bg-blue-600 border border-blue-600 transition-colors duration-150 p-3 font-semibold text-blue-600 hover:text-white inline-flex items-center rounded"
           >
-            <span aria-hidden="true">&#8722;</span>
+            {IconMinus}
           </button>
           <div className="w-12 text-center font-semibold">{contador}</div>
           <button
+            type="button"
             onClick={add}
             aria-label="Agregar"
-            className="bg-blue-600 text-white w-12 h-12 rounded shadow"
+            className="bg-white hover:bg-blue-600 border border-blue-600 transition-colors duration-150 p-3 font-semibold text-blue-600 hover:text-white inline-flex items-center rounded"
           >
-            <span aria-hidden="true">&#43;</span>
+            {IconPlus}
           </button>
         </div>
         <p className="ml-4">Stock disponible: {stock}</p>
@@ -40,11 +43,11 @@ function ItemCount({ stock, initial, onAdd }) {
 
       <button
         onClick={() => onAdd(contador)}
-        className="bg-blue-600 text-white font-semibold block p-4 rounded shadow w-full sm:w-auto"
+        className="bg-blue-600 text-white font-semibold p-4 rounded shadow transition-colors duration-150 hover:bg-blue-700 outline outline-offset-2 focus:bg-blue-700"
       >
         Agregar al carro
       </button>
-    </div>
+    </>
   );
 }
 
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -6,18 +6,21 @@ export default function ItemDetail({ producto, onAdd, addedToCart }) {
   const { imageUrl, title, price, description, stock } = producto;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 pt-8">
       <div>
         <img src={imageUrl} alt={title} />
       </div>
       <div>
-        <h1 className="text-2xl font-semibold">{title}</h1>
+        <h1 className="text-2xl font-semibold mb-2">{title}</h1>
         <p className="text-xl mb-4">$ {price && formatPrice(price)}</p>
         <p className="mb-4">{description}</p>
         {addedToCart ? (
-          <div>
-            <Link to="/cart">Terminar mi compra</Link>
-          </div>
+          <Link
+            to="/cart"
+            className="inline-flex items-center bg-blue-600 text-white font-semibold p-4 rounded shadow transition-colors duration-150 hover:bg-blue-700 outline outline-offset-2 focus:bg-blue-700"
+          >
+            Terminar mi compra
+          </Link>
         ) : (
           <ItemCount stock={stock} initial={1} onAdd={onAdd} />
         )}
