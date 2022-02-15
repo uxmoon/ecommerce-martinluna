4e3efc27c63fc7857d98583acee2f026a7087527
Martin Luna <maluna@technisys.com>
2022-02-15 12:30:48 -0300

    Estilos para formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -60,77 +60,124 @@ export default function Form({ cart, totalPrice }) {
         </>
       )}
       {cart.length > 0 && !orderId && (
-        <>
-          <h3>Datos del comprador</h3>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="userName">Nombre y apellido</label>
-              <input
-                required
-                type="text"
-                name="userName"
-                id="userName"
-                ref={userNameRef}
-              />
-            </div>
-            <div>
-              <label htmlFor="userEmail">Email</label>
-              <input
-                required
-                type="email"
-                name="userEmail"
-                id="userEmail"
-                ref={userEmailRef}
-              />
-            </div>
-            <div>
-              <label htmlFor="userAddress">Domicilio</label>
-              <input
-                required
-                type="text"
-                name="userAddress"
-                id="userAddress"
-                ref={userAddressRef}
-              />
-            </div>
-            <div>
-              <label htmlFor="userCity">Ciudad</label>
-              <input
-                required
-                type="text"
-                name="userCity"
-                id="userCity"
-                ref={userCityRef}
-              />
-            </div>
-            <div>
-              <label htmlFor="userState">Estado o provincia</label>
-              <input
-                required
-                type="text"
-                name="userState"
-                id="userState"
-                ref={userStateRef}
-              />
-            </div>
-            <div>
-              <label htmlFor="userPhone">Teléfono</label>
-              <input
-                required
-                type="tel"
-                name="userPhone"
-                id="userPhone"
-                ref={userPhoneRef}
-                placeholder="Ej: (011) 2222-3333"
-              />
-            </div>
-            <div>
-              <button className="bg-blue-600 text-white font-semibold px-4 py-3 rounded shadow transition-colors duration-150 hover:bg-blue-700 outline outline-offset-2 focus:bg-blue-700">
-                Finalizar compra
-              </button>
-            </div>
-          </form>
-        </>
+        <div className="md:grid grid-cols-3 gap-4 bg-slate-100 p-4 rounded-md">
+          <div>
+            <h3 className="text-xl sm:text-2xl font-bold">
+              Datos del comprador
+            </h3>
+            <p className="mb-4">
+              Para generar la orden necesitamos los siguientes datos.
+            </p>
+          </div>
+          <div className="col-span-2">
+            <form onSubmit={handleSubmit}>
+              <div className="md:grid grid-cols-2 gap-4">
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userName"
+                  >
+                    Nombre y apellido
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="text"
+                    name="userName"
+                    id="userName"
+                    ref={userNameRef}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userEmail"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="email"
+                    name="userEmail"
+                    id="userEmail"
+                    ref={userEmailRef}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userAddress"
+                  >
+                    Domicilio
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="text"
+                    name="userAddress"
+                    id="userAddress"
+                    ref={userAddressRef}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userCity"
+                  >
+                    Ciudad
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="text"
+                    name="userCity"
+                    id="userCity"
+                    ref={userCityRef}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userState"
+                  >
+                    Estado o provincia
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="text"
+                    name="userState"
+                    id="userState"
+                    ref={userStateRef}
+                  />
+                </div>
+                <div>
+                  <label
+                    className="block mb-2 text-neutral-800"
+                    htmlFor="userPhone"
+                  >
+                    Teléfono
+                  </label>
+                  <input
+                    className="w-full mb-4 rounded px-4 py-2 border border-gray-300"
+                    required
+                    type="tel"
+                    name="userPhone"
+                    id="userPhone"
+                    ref={userPhoneRef}
+                    placeholder="Ej: (011) 2222-3333"
+                  />
+                </div>
+                <div className="md:text-right col-span-2">
+                  <button className="bg-blue-600 text-white font-semibold px-4 py-3 rounded shadow transition-colors duration-150 hover:bg-blue-700 outline outline-offset-2 focus:bg-blue-700 w-full md:w-auto ">
+                    Finalizar compra
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       )}
     </div>
   );
