88fb057923892dfe372d5d4e6cd9415158555c69
Martin Luna <maluna@technisys.com>
2022-02-18 18:51:34 -0300

    Se agrega footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Cart from "./components/Cart";
@@ -10,24 +11,27 @@ function App() {
   return (
     <CartProvider>
       <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <ItemListContainer />
-          </Route>
-          <Route exact path="/category/:categoryId">
-            <ItemListContainer />
-          </Route>
-          <Route exact path="/item/:itemId">
-            <ItemDetailContainer />
-          </Route>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/cart/:cartId">
-            <Checkout />
-          </Route>
-        </Switch>
+        <div className="flex flex-col h-screen">
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <ItemListContainer />
+            </Route>
+            <Route exact path="/category/:categoryId">
+              <ItemListContainer />
+            </Route>
+            <Route exact path="/item/:itemId">
+              <ItemDetailContainer />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+            <Route exact path="/cart/:cartId">
+              <Checkout />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
       </BrowserRouter>
     </CartProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="mt-auto footer py-4 bg-gray-100">
+      <p className="text-center">footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
