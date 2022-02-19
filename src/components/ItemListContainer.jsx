4636b96a172edb569937fc9929efa6cb8b5b2f1b
Martin Luna <maluna@technisys.com>
2022-02-19 20:22:24 -0300

    Se agrega Hero en home

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import ItemList from "./ItemList";
+import Hero from "./Hero";
 
 const ItemListContainer = () => {
   return (
     <main className="max-w-7xl mx-auto px-4 pt-8 pb-16">
+      <Hero />
       <ItemList />
     </main>
   );
