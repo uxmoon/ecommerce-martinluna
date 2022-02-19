f2ba95b00c511771158fb8b7b4e2430209bf5a9b
Martin Luna <maluna@technisys.com>
2022-02-19 19:51:28 -0300

    Estilos para card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,12 +5,18 @@ function Item({ producto }) {
   const { id, imageUrl, title, price } = producto;
 
   return (
-    <div className="mx-auto">
-      <Link to={`/item/${id}`}>
-        <img src={imageUrl} alt={title} className="object-contain h-48 w-96" />
-        <h2>{title}</h2>
-      </Link>
-      <p>$ {formatPrice(price)}</p>
+    <div className="transform -skew-y-3 bg-white p-8 border shadow-md">
+      <div className="transform skew-y-3">
+        <Link to={`/item/${id}`}>
+          <img
+            src={imageUrl}
+            alt={title}
+            className="object-contain h-48 w-96 mb-4"
+          />
+          <h2 className="text-xl font-semibold">{title}</h2>
+        </Link>
+        <p className="text-lg text-gray-800">$ {formatPrice(price)}</p>
+      </div>
     </div>
   );
 }
