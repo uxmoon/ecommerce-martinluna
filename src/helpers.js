4332ffadd9c100bfe26dcc019e54beb40507f8ce
Martin Luna <maluna@technisys.com>
2022-02-20 13:37:23 -0300

    Formato para lowercase y quitar tildes

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,10 @@
 export function formatPrice(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export function formatLink(string) {
+  return string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
