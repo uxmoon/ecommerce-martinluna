da44f5e6e14efab5992b731f747018db895e58d0
Martin Luna <maluna@technisys.com>
2022-02-24 12:10:40 -0300

    Se agregan estilos para boton primario y secundario

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-export default function Button({ children, onPress }) {
+export default function Button(props) {
+  const { children, onPress, variant, ...rest } = props;
+
+  const secondaryStyles =
+    "bg-white hover:bg-indigo-600 border border-indigo-600 transition-colors duration-150 p-3 font-semibold text-indigo-600 hover:text-white inline-flex items-center rounded";
+  const primaryStyles =
+    "bg-indigo-600 text-white font-semibold px-4 py-3 rounded shadow transition-colors duration-150 hover:bg-indigo-700 w-full md:w-auto";
+
   return (
-    <button onClick={onPress} variant>
+    <button
+      type="button"
+      onClick={onPress}
+      className={variant === "secondary" ? secondaryStyles : primaryStyles}
+      {...rest}
+    >
       {children}
     </button>
   );
