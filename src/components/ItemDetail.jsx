b6b9ddb0460703fdba33dce807576c656f507a1d
Martin Luna <maluna@technisys.com>
2022-02-24 12:16:35 -0300

    Implementacion ButtonLink

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import ItemCount from "./ItemCount";
 import { formatPrice } from "../helpers";
+import ButtonLink from "./ButtonLink";
 
 export default function ItemDetail({ product, onAdd, addedToCart }) {
   const { imageUrl, title, price, description, stock } = product;
@@ -20,18 +20,8 @@ export default function ItemDetail({ product, onAdd, addedToCart }) {
         <p className="mb-4">{description}</p>
         {addedToCart ? (
           <>
-            <Link
-              to="/cart"
-              className="w-full sm:w-auto inline-flex items-center justify-center bg-indigo-600 text-white font-semibold px-4 py-3 rounded shadow transition-colors duration-150 hover:bg-indigo-700 mb-4 sm:mr-4 border border-indigo-600"
-            >
-              Terminar mi compra
-            </Link>
-            <Link
-              to="/"
-              className="w-full sm:w-auto inline-flex items-center justify-center bg-white hover:bg-indigo-600 border border-indigo-600 transition-colors duration-150 px-4 py-3 font-semibold text-indigo-600 hover:text-white rounded"
-            >
-              Seguir comprando
-            </Link>
+            <ButtonLink path="/cart">Terminar mi compra</ButtonLink>
+            <ButtonLink path="/" variant="secondary">Seguir comprando</ButtonLink>
           </>
         ) : (
           <ItemCount stock={stock} initial={1} onAdd={onAdd} />
